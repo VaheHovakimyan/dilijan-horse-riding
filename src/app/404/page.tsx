8e690df9fe57b1bf021d5192b2b449f4223f0135
Vahe4836 <88284245+Vahe4836@users.noreply.github.com>
2024-06-19 00:20:11 +0400

    new divs

--- src/app/404/page.tsx
+++ src/app/404/page.tsx
@@ -1,21 +1,22 @@
-import styles from './api/PageStyle.module.scss'
-import React from "react";
-import { useRef, useEffect, useState } from "react";
+// import styles from './api/PageStyle.module.scss'
+// import React from "react";
+// import { useRef, useEffect, useState } from "react";
 
-import cl from "classnames";
-import style from "../../libs/common/ui/pages/AboutPage/Map/Map.module.scss";
-import ArrowRightIcon from "@/libs/common/ui/assets/imgs/Group 21.png";
+// import cl from "classnames";
+// import style from "../../libs/common/ui/pages/AboutPage/Map/Map.module.scss";
+// import ArrowRightIcon from "@/libs/common/ui/assets/imgs/Group 21.png";
 
-import Link from "next/link";
+// import Link from "next/link";
 
 export const Custom404 = () => {
 
-    const myElementRef = useRef(null);
+    // const myElementRef = useRef(null);
 
 
     return(
-        <div className={styles._404_div}>
-            <div className="container">
+        <div >
+          404  
+            {/* <div className="container">
                 <div className="row" ref={myElementRef}>
           
                     <div className="col-md-6 align-self-center">
@@ -33,7 +34,7 @@ export const Custom404 = () => {
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 }
