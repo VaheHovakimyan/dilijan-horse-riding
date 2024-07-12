7b2b8ed9565be7e653378dbc39116e0674a02683
Vahe4836 <88284245+Vahe4836@users.noreply.github.com>
2024-07-12 23:27:16 +0400

    icons change and some functionality for responsive design

--- src/app/_lib/common/ui/pages/HomePageNew/galleryHome/GalleryHome.tsx
+++ src/app/_lib/common/ui/pages/HomePageNew/galleryHome/GalleryHome.tsx
@@ -8,28 +8,32 @@ import horse_image from '../../../assets/images/horse_image.jpg';
 export const GalleryHome = ({ }: {}) => {
 
   return (
-    <div className={style.gallery_home}>
-      <div className={style.gallery_home_main_1}>
-        <div className={style.gallery_home_main_item}>
-          <div className={style.gallery_home_main_item_opacity}></div>
+    <div className={style.gallery_home_main}>
+      <div className={style.gallery_home}>
+
+        <div className={style.gallery_home_main_1}>
+          <div className={style.gallery_home_main_item}>
+            <div className={style.gallery_home_main_item_opacity}></div>
+          </div>
+          <div className={style.gallery_home_main_item}>
+            <div className={style.gallery_home_main_item_opacity}></div>
+          </div>
+          <div className={style.gallery_home_main_item}>
+            <div className={style.gallery_home_main_item_opacity}></div>
+          </div>
         </div>
-        <div className={style.gallery_home_main_item}>
-          <div className={style.gallery_home_main_item_opacity}></div>
-        </div>
-        <div className={style.gallery_home_main_item}>
-          <div className={style.gallery_home_main_item_opacity}></div>
-        </div>
-      </div>
-      <div className={style.gallery_home_main_2}>
-        <div className={style.gallery_home_main_item}>
-          <div className={style.gallery_home_main_item_opacity}></div>
-        </div>
-        <div className={style.gallery_home_main_item}>
-          <div className={style.gallery_home_main_item_opacity}></div>
-        </div>
-        <div className={style.gallery_home_main_item}>
-          <div className={style.gallery_home_main_item_opacity}>
-            <p className={style.gallery_home_main_item_opacity_last}>+ 343</p>
+
+        <div className={style.gallery_home_main_2}>
+          <div className={style.gallery_home_main_item}>
+            <div className={style.gallery_home_main_item_opacity}></div>
+          </div>
+          <div className={style.gallery_home_main_item}>
+            <div className={style.gallery_home_main_item_opacity}></div>
+          </div>
+          <div className={style.gallery_home_main_item}>
+            <div className={style.gallery_home_main_item_opacity}>
+              <p className={style.gallery_home_main_item_opacity_last}>+ 343</p>
+            </div>
           </div>
         </div>
       </div>
