dd43c29037c62107256ae66c876905f048b5073a
Mayis <mayis11.98@gmail.com>
2025-02-02 23:38:39 +0400

    ads

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone", // Enables a single executable for the server
+  output: "standalone", // Keeps the app bundled into a single executable for production
   reactStrictMode: true,
-  experimental: {
-    serverActions: true, // Needed for server-side actions in Next.js 14
-  },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
